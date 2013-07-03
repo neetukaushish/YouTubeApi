1325a56d200e3c4861d648ac94925a943350a690
neetu <neetusharma0204@gmail.com>
2013-07-03 11:48:15 +0530

    Replace table layout with list view

--- src/com/addvalsolutions/youtubeapi/MainActivity.java
+++ src/com/addvalsolutions/youtubeapi/MainActivity.java
@@ -1,28 +1,23 @@
 package com.addvalsolutions.youtubeapi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayer.Provider;
-import com.google.android.youtube.player.YouTubePlayerView;
 
 public class MainActivity extends Activity {
 
@@ -39,26 +34,48 @@ public class MainActivity extends Activity {
 		/*edit=(EditText)findViewById(R.id.edit_search);
 		btn=(Button)findViewById(R.id.btn_search);
 		*/
-		tb=(TableLayout)findViewById(R.id.tableLayout1);
+		//tb=(TableLayout)findViewById(R.id.tableLayout1);
 		//img=(ImageView)findViewById(R.id.img);
 		//tv=(TextView)findViewById(R.id.tv_name1);
+		
+		ArrayList<HashMap<String, String>> items=new ArrayList<HashMap<String,String>>();
 		
 		try {
 			JSONParser obj=new JSONParser();
 			JSONArray jsonarray=obj.getVideos(title);
-			for(int i=0;i<jsonarray.length();i++){
-				
-				View row=getLayoutInflater().inflate(R.layout.activity_main, null);
-				((TextView)row.findViewById(R.id.tv_name1)).setText(jsonarray.getJSONObject(i).getString("title"));
-				((ImageView)row.findViewById(R.id.img)).setVisibility(i);
-				tb.addView(row);
-				
-			
+			for(int i=0;i<jsonarray.length();i++){	
+				String title=jsonarray.getJSONObject(i).getString("title");
+				JSONObject thumb=jsonarray.getJSONObject(i).getJSONObject("thumbnail");
+				String sqDefault=thumb.getString("sqDefault");
+				JSONObject player=jsonarray.getJSONObject(i).getJSONObject("player");
+				String player_default=player.getString("default");
+				//creating hashmap
+			    HashMap<String, String> map=new HashMap<String, String>();
+			    
+			    //adding each child node to HashMap key and value
+			    map.put("title", title);
+			    map.put("sqDefault", sqDefault);
+                map.put("player", player_default);
+                
+                //adding to arraylist
+                items.add(map);
+                
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//Updating parse data into list view
+		
+				ListAdapter adapter = new SimpleAdapter(this, items,
+						R.layout.list_items,
+						new String[] { "sqDefault","player" }, new int[] {
+								R.id.img, R.id.tv_search });
+
+				ListView lv=(ListView)findViewById(R.id.list);
+				lv.setAdapter(adapter);
+
+				
 	}
 
 	@Override
