577052a44a15f13606cc009e315fe80bd5ab74b2
neetu <neetusharma0204@gmail.com>
2013-07-03 17:41:24 +0530

    play video on click of an item in listview

--- src/com/addvalsolutions/youtubeapi/MainActivity.java
+++ src/com/addvalsolutions/youtubeapi/MainActivity.java
@@ -2,14 +2,25 @@ package com.addvalsolutions.youtubeapi;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Video;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -22,23 +33,21 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 
-	TableLayout tb;
 	ImageView img;
-	TableRow tr;
+	EditText edit;
+	Button btn;
 	TextView tv;
 	String title="cars";
+	String player_default="";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		/*edit=(EditText)findViewById(R.id.edit_search);
+		edit=(EditText)findViewById(R.id.edit_search);
 		btn=(Button)findViewById(R.id.btn_search);
-		*/
-		//tb=(TableLayout)findViewById(R.id.tableLayout1);
-		//img=(ImageView)findViewById(R.id.img);
-		//tv=(TextView)findViewById(R.id.tv_name1);
 		
-		ArrayList<HashMap<String, String>> items=new ArrayList<HashMap<String,String>>();
+		
+		final ArrayList<HashMap<String, String>> items=new ArrayList<HashMap<String,String>>();
 		
 		try {
 			JSONParser obj=new JSONParser();
@@ -48,13 +57,13 @@ public class MainActivity extends Activity {
 				JSONObject thumb=jsonarray.getJSONObject(i).getJSONObject("thumbnail");
 				String sqDefault=thumb.getString("sqDefault");
 				JSONObject player=jsonarray.getJSONObject(i).getJSONObject("player");
-				String player_default=player.getString("default");
+				player_default=player.getString("default");
 				//creating hashmap
 			    HashMap<String, String> map=new HashMap<String, String>();
 			    
 			    //adding each child node to HashMap key and value
 			    map.put("title", title);
-			    map.put("sqDefault", sqDefault);
+			   // map.put("sqDefault", sqDefault);
                 map.put("player", player_default);
                 
                 //adding to arraylist
@@ -69,12 +78,33 @@ public class MainActivity extends Activity {
 		
 				ListAdapter adapter = new SimpleAdapter(this, items,
 						R.layout.list_items,
-						new String[] { "sqDefault","player" }, new int[] {
-								R.id.img, R.id.tv_search });
+						new String[] { "sqDefault","title" }, new int[] {
+								R.id.img , R.id.tv_search});
 
-				ListView lv=(ListView)findViewById(R.id.list);
+				final ListView lv=(ListView)findViewById(R.id.list);
 				lv.setAdapter(adapter);
 
+lv.setOnItemClickListener(new OnItemClickListener() {
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		/*Video chosen=(Video)arg0.getItemAtPosition(position);
+		int i=Integer.parseInt(chosen.toString());
+		lv.SelectedItems[i].SubItems[1].Text;*/
+		Map<String, String> map=(Map<String,String>)((ListView)lv).getItemAtPosition(position);
+		String val=map.get("player");
+		startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(val)));
+	}
+});
+				btn.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://m.youtube.com/details?v=urHuO7Zbhhw")));
+					}
+				});
 				
 	}
 
